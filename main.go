531995c3ba1b52a5add640df3ea335a02abd57d8
Sergio-M-A <misyurins@gmail.com>
2021-06-02 18:28:14 +0300

    second

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+var(
+url2 = "https://jsonplaceholder.typicode.com/posts"
+)
 
 func main()  {
 	fmt.Println("hey there")
+	for i:=1; i<10; i++ {
+
+		go getUrlmy(i)
+	}
+	var input string
+	fmt.Scanln(&input)
+
 }
+
+func getUrlmy(j int){
+
+	strk := fmt.Sprint(j)
+	url2 = "https://jsonplaceholder.typicode.com/posts/" + strk
+
+	res, err := http.Get(url2)
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	fmt.Println()
+	bodyString := string(body)
+	fmt.Print(bodyString)
+	//amt := time.Duration(rand.Intn(500))
+	//time.Sleep(time.Millisecond * amt)
+}
+
+
